0cc84fd25f828ecc841bb682f96578520cfdbf90
TatyanaTropkina <tropkina.tatyana@gmail.com>
2022-11-04 21:30:46 +0300

    add components and file js

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+function Header() {
+	return (
+		<header className="header">
+			<img src="https://res-1.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_256,w_256,f_auto,q_auto:eco/wdhrn7hbqx5wzzhxyyw4" alt="hh" />
+		</header>
+	)
+}
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,25 @@
+function Profile() {
+	return (
+		<div className="content">
+			<div>
+				<img src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg" alt="Japan" />
+			</div>
+			<div>
+				ava + des
+				<img src="https://www.portphillip.vic.gov.au/media/l4bc2xgo/copp_local-laws_pets-of-port-phillip_web-banner_0622.jpg?center=0.57191619158284612,0.52646229440739711&mode=crop&width=710&height=710&rnd=133005394387900000" alt="" />
+			</div>
+			<div>
+				my posts
+				<div>new post</div>
+				<div>
+					<div>
+						post 1
+					</div>
+					<div>post 2</div>
+
+				</div>
+			</div>
+		</div>
+	)
+}
+export default Profile;
