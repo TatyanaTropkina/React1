f60cb782882d222f436df84cd7de465aa38b9c6e
TatyanaTropkina <tropkina.tatyana@gmail.com>
2022-11-16 22:00:51 +0300

    add reducer

--- src/redux/dialogs-reducer/dialogs-reducer.js
+++ src/redux/dialogs-reducer/dialogs-reducer.js
@@ -1,16 +1,31 @@
 const SEND_MESSAGE = "SEND-MESSAGE";
 const UPDATE_NEW_MESSAGE_BODY = "UPDATE-NEW-MESSAGE-BODY";
 const dialogsReducer = (state, action) => {
-    if (action.type === SEND_MESSAGE) {
-        let body = state.newMessageBody;
-        state.newMessageBody = '';
-        state.messages.push({id: 6, message: body});
-
-    } else if (action.type === UPDATE_NEW_MESSAGE_BODY) {
-
-        state.newMessageBody = action.body;
+    switch(action.type) {
+        case SEND_MESSAGE:
+            let body = state.newMessageBody;
+            state.newMessageBody = '';
+            state.messages.push({id: 6, message: body});
+            return state;
+        case UPDATE_NEW_MESSAGE_BODY:
+            state.newMessageBody = action.body;
+            return state;
+        default:
+            return state;
     }
 
-    return state;
+
+}
+export const sendMessageCreator = () => {
+
+    return {
+        type: SEND_MESSAGE
+    }
+}
+export const updateNewMessageBodyCreator = (text) => {
+    return {
+        type: UPDATE_NEW_MESSAGE_BODY,
+        body: text
+    }
 }
 export default dialogsReducer;
--- src/redux/profile-reducer/profile-reducer.js
+++ src/redux/profile-reducer/profile-reducer.js
@@ -2,21 +2,33 @@ const ADD_POST = "ADD-POST";
 const UPDATE_NEW_POST_TEXT = "UPDATE-NEW-POST-TEXT";
 
 const profileReducer = (state, action) => {
-    if(action.type === ADD_POST) {
-        let newPost = {
-            id: 5,
-            // сообщение сидит в newPostText в state
-            message: state.newPostText,
-            likesCount:0
-        };
-      state.posts.push(newPost);
-     state.newPostText = '';
+    switch(action.type) {
+        case ADD_POST:
+            let newPost = {
+                id: 5,
+                // сообщение сидит в newPostText в state
+                message: state.newPostText,
+                likesCount:0
+            };
+            state.posts.push(newPost);
+            state.newPostText = '';
+            return state;
+        case UPDATE_NEW_POST_TEXT:
+            state.newPostText = action.newText;
+            return state;
+        default:
+            return state;
     }
-    else if (action.type === UPDATE_NEW_POST_TEXT) {
-        state.newPostText = action.newText;
+}
+export const addPostActionCreator = () => {
+    return {
+        type: ADD_POST
     }
-
-
-    return state;
+}
+export const updateNewPostTextActionCreator = (text) => {
+    return {
+        type: UPDATE_NEW_POST_TEXT,
+        newText: text
+    }
 }
 export default profileReducer;
